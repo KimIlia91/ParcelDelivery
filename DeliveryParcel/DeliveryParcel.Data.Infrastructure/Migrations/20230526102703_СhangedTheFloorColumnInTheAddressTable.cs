fb7628a72d61984c1fc39df246727b6e6fc18d6a
IliaKim91 <paterfamelias@gmail.com>
2023-05-26 21:58:43 +0600

    Added migration

---
+++ DeliveryParcel/DeliveryParcel.Data.Infrastructure/Migrations/20230526102703_СhangedTheFloorColumnInTheAddressTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryParcel.Data.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class СhangedTheFloorColumnInTheAddressTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Flat",
+                table: "Addresses",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Flat",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
